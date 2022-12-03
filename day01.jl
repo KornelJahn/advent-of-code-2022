7c1f2591dd4baf9eb7f53a006aefe29c5b3e5929
Kornel Jahn <kjahn.public@gmail.com>
2022-12-03 16:08:44 +0100

    day01: Simplify parsing

--- day01.jl
+++ day01.jl
@@ -3,49 +3,26 @@ using Test
 abstract type PuzzlePartTrait end
 struct Part{N} <: PuzzlePartTrait end
 
-AbstractLines = AbstractVector{<:AbstractString}
-
-"""
-Iterate over the sums of calories for each elf.
-"""
-function iter_sums(lines::AbstractLines)
-    total::Int = 0
-    Channel() do channel
-        for line in lines
-            if isempty(line)
-                push!(channel, total)
-                total = 0
-            else
-                total += parse(typeof(total), line)
-            end
-        end
-        if total > 0
-            push!(channel, total)
-        end
-    end
+"""Parse input into elf-wise sums of calories."""
+function parse_input(input::AbstractString)
+    groupwise = split(input, "\n\n")
+    convert(group) = parse.(Ref(Int), split(strip(group), "\n"))
+    return sum.(convert.(groupwise))
 end
 
-"""Find the maximal sum of calories."""
-find_maximal_sum(lines::AbstractLines) = maximum(iter_sums(lines))
+AbstractSums = AbstractVector{<:Integer}
 
-"""
-Find the three greatest sums of calories.
-"""
-function find_three_greatest_sums(lines::AbstractLines)
-    sort(collect(iter_sums(lines)), rev=true)[1:3]
+function solve(::Part{1}, sums::AbstractSums)
+    return maximum(sums)
 end
 
-function solve(::Part{1}, lines::AbstractLines)
-    return find_maximal_sum(lines)
-end
-
-function solve(::Part{2}, lines::AbstractLines)
-    return sum(find_three_greatest_sums(lines))
+function solve(::Part{2}, sums::AbstractSums)
+    return sum(sort(sums, rev=true)[1:3])
 end
 
 function main()
     for part_idx in (1, 2)
-        solution = solve(Part{part_idx}(), readlines(ARGS[1], keep=false))
+        solution = solve(Part{part_idx}(), parse_input(read(ARGS[1], String)))
         println("Solution (part $part_idx): $solution")
     end
 end
@@ -67,9 +44,9 @@ function test()
 
         10000
         """
-    lines = split(input, keepempty=true)[begin:(end-1)]
-    @test solve(Part{1}(), lines) == 24000
-    @test solve(Part{2}(), lines) == 45000
+    parsed = parse_input(input)
+    @test solve(Part{1}(), parsed) == 24000
+    @test solve(Part{2}(), parsed) == 45000
 end
 
 if length(ARGS) > 0
