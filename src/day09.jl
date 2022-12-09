bd43e5f6eea105961ee1b8796cd8177b2de2a117
Kornel Jahn <kjahn.public@gmail.com>
2022-12-09 17:04:01 +0100

    Revise day 9 solution (WIP, part 2 fails)

--- src/day09.jl
+++ src/day09.jl
@@ -5,47 +5,97 @@ export parse_input, solve
 using ..Puzzle: Day, Part
 import ..Puzzle: parse_input, solve
 
-AbstractInput = AbstractVector{<:Tuple{<:AbstractChar, <:Integer}}
+AbstractInput = AbstractVector{
+    <:Tuple{<:Tuple{<:Integer, <:Integer}, <:Integer}
+}
+
+const DIRECTIONS = Dict(
+    'U' => (0, 1),
+    'D' => (0, -1),
+    'L' => (-1, 0),
+    'R' => (1, 0),
+)
 
 function parse_input(::Day{9}, raw::AbstractString)
     function convert_line(l)
         (rawdir, rawcnt) = split(l)
-        return Char(rawdir[1]), parse(Int, rawcnt)
+        return DIRECTIONS[rawdir[1]], parse(Int, rawcnt)
     end
     lines = split(strip(raw), '\n')
     return convert_line.(lines)
 end
 
-# FIXME: does not give correct answer for real input!
 function solve(::Day{9}, ::Part{1}, input::AbstractInput)
-    return length(unique(tail_positions(input)))
+    head_history = head_positions(input)
+    return length(unique(tail_positions(head_history)))
 end
 
-function solve(::Day{9}, ::Part{2}, input::AbstractInput)
-    return nothing
-end
-
-function tail_positions(input::AbstractInput)
-    DIRECTIONS = Dict(
-        'U' => [0, 1],
-        'D' => [0, -1],
-        'L' => [-1, 0],
-        'R' => [1, 0],
+# FIXME: this logic is unfortunately wrong
+function solve(::Day{9}, ::Part{2}, input::AbstractInput; taillen::Int = 9)
+    head_history = head_positions(input)
+    tail_histories = accumulate(
+        (hist, _)->tail_positions(hist),
+        1:taillen,
+        init=head_history
     )
-    tail_positions = Vector{Vector{Int}}()
-    head_position = [0, 0]
-    for (dir, cnt) in input
-        append!(
-            tail_positions,
-            accumulate(
-                .+,
-                Iterators.repeat(DIRECTIONS[dir], cnt - 1),
-                init=head_position
-            )
-        )
-        head_position += DIRECTIONS[dir]
+    print_rope_history(head_history, tail_histories...)
+    return length(unique(tail_histories[end]))
+end
+
+AbstractPos = Tuple{<:Integer, <:Integer}
+
+function tail_positions(head_history::AbstractVector{<:AbstractPos})
+    tail_history = [(0, 0)]
+    for (head_prev, head) in zip(head_history, head_history[2:end])
+        push!(tail_history, next_tail(tail_history[end], head_prev, head))
     end
-    return tail_positions
+    return tail_history
+end
+
+function head_positions(input::AbstractInput)
+    heads = [(0, 0)]
+    append!(
+        heads,
+        accumulate(
+            .+,
+            Iterators.flatten(map(args->Iterators.repeated(args...), input)),
+            init=heads[end]
+        )
+    )
+    return heads
+end
+
+function next_tail(
+    tail::AbstractPos,
+    head_prev::AbstractPos,
+    head::AbstractPos,
+)
+    return distance_squared(head, tail) > 2 ? head_prev : tail
+end
+
+distance_squared(p::AbstractPos, q::AbstractPos) = sum((p .- q).^2)
+
+function print_rope_history(histories...)
+    CHARS = "987654321H"
+    # FIXME: fixed size based on global extrema
+    for step in 1:length(histories[1])
+        points = [h[step] for h in Iterators.reverse(histories)]
+        (xmin, xmax) = extrema(p[1] for p in points)
+        (ymin, ymax) = extrema(p[2] for p in points)
+        Nx = xmax - xmin + 1
+        Ny = ymax - ymin + 1
+        A = fill('.', Ny, Nx)
+        A[-ymin + 1, -xmin + 1] = 's'
+        for (i, (x, y)) in enumerate(points)
+            A[y - ymin + 1, x - xmin + 1] = CHARS[i]
+        end
+        A = reverse(A, dims=1)
+        println("Step $step\n")
+        for row in eachrow(A)
+            println(prod(row))
+        end
+        println()
+    end
 end
 
 end # module
