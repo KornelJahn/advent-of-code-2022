1ce74494a7ac2cd1fe77f84bebc63802ecf95145
Kornel Jahn <kjahn.public@gmail.com>
2022-12-09 06:56:20 +0100

    Generate repeated `include("dayN.jl"); using .DayN` statements by a macro

--- src/AdventOfCode2022.jl
+++ src/AdventOfCode2022.jl
@@ -1,52 +1,25 @@
-include("puzzle.jl")
-include("day01.jl")
-include("day02.jl")
-include("day03.jl")
-include("day04.jl")
-include("day05.jl")
-include("day06.jl")
-include("day07.jl")
-include("day08.jl")
-# include("day09.jl")
-# include("day10.jl")
-# include("day11.jl")
-# include("day12.jl")
-# include("day13.jl")
-# include("day14.jl")
-# include("day15.jl")
-# include("day16.jl")
-# include("day17.jl")
-# include("day18.jl")
-# include("day19.jl")
-# include("day20.jl")
-# include("day21.jl")
-# include("day22.jl")
-# include("day23.jl")
-# include("day24.jl")
-# include("day25.jl")
+using Printf: @sprintf
 
-using .Day1
-using .Day2
-using .Day3
-using .Day4
-using .Day5
-using .Day6
-using .Day7
-using .Day8
-# using .Day9
-# using .Day10
-# using .Day11
-# using .Day12
-# using .Day13
-# using .Day14
-# using .Day15
-# using .Day16
-# using .Day17
-# using .Day18
-# using .Day19
-# using .Day20
-# using .Day21
-# using .Day22
-# using .Day23
-# using .Day24
-# using .Day25
+"""
+Generate include calls and using statements for available daily solutions.
+"""
+macro days()
+    function make_ex(i::Integer)
+        d = @sprintf("%02d", i)
+        p = "$(@__DIR__)/day$d.jl"
+        if isfile(p)
+            return (
+                Expr(:call, :include, p),
+                Expr(:using, Expr(:., :., Symbol("Day$i"))),
+            )
+        else
+            return nothing
+        end
+    end
+
+    puzzle_mod_path = "$(@__DIR__)/puzzle.jl"
+    exs = Iterators.flatten(filter(!isnothing, make_ex.(1:25)))
+    return Expr(:toplevel, Expr(:call, :include, puzzle_mod_path), exs...)
+end
+
+@days
