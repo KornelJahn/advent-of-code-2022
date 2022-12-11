6d45ceb75e6f021fe5ebf05420d9be41c34d7b29
Kornel Jahn <kjahn.public@gmail.com>
2022-12-11 07:26:36 +0100

    Update test template

--- test/template.jl
+++ test/template.jl
@@ -5,12 +5,12 @@
             """
         )
 
-        # @test DayNN.solve_part1(input) ==
+        @test DayNN.solve_part1(input) ==
         # @test DayNN.solve_part2(input) ==
     end
 
     @testset "Real input" begin
-        input = DayNN.parse_input(load_input("dayNN.txt"))
+        # input = DayNN.parse_input(load_input("dayNN.txt"))
 
         # @test DayNN.solve_part1(input) ==
         # @test DayNN.solve_part2(input) ==
