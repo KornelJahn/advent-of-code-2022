b6c2397930d39211f5ffce57af7650677c5b8eaa
Kornel Jahn <kjahn.public@gmail.com>
2022-12-14 10:04:34 +0100

    day13: Introduce a fast and a slower tree-based solution

--- src/day13.jl
+++ src/day13.jl
@@ -1,9 +1,85 @@
 module Day13
 
-function parse_input(raw::AbstractString)
+function parse_input(raw::AbstractString, convert_string::Function)
     strings = split(replace(strip(raw), "\n\n" => "\n"), '\n')
     return convert_string.(strings)
 end
+
+function solve_part1(packets, lessthan::Function)
+    pairs = Iterators.partition(packets, 2)
+    flags = [lessthan(pair...) for pair in pairs]
+    return sum(findall(flags))
+end
+
+function solve_part2(packets, convert_string::Function, lessthan::Function)
+    dividers = convert_string.(["[[2]]", "[[6]]"])
+    extended = vcat(packets, dividers)
+    sorted = sort(extended, lt=lessthan)
+    return prod(findfirst(==(divider), sorted) for divider in dividers)
+end
+
+const OPEN = -1
+const CLOSE = -2
+
+function convert_string(string::AbstractString)
+    preprocessed = string |>
+        strip |>
+        s->replace(s, "[" => "$OPEN ", "]" => " $CLOSE", "," => " ") |>
+        split
+    return parse.(Int, preprocessed)
+end
+
+function lessthan(left::T, right::T) where {T<:AbstractVector{<:Integer}}
+    ldq = reverse(left)
+    rdq = reverse(right)
+    while !isempty(ldq) && !isempty(rdq)
+        # Pop next symbol from each packet
+        l = pop!(ldq)
+        r = pop!(rdq)
+
+        if l == r
+            continue
+        elseif l == OPEN && r != OPEN
+            if r == CLOSE
+                # Exhaustion case, equivalent to: [[]] > []
+                return false
+            end
+            # Push l
+            push!(ldq, l)
+            # Push [r]
+            push!(rdq, CLOSE)
+            push!(rdq, r)
+            push!(rdq, OPEN)
+        elseif r == OPEN && l != OPEN
+            if l == CLOSE
+                # Exhaustion case, equivalent to: [] < [[]]
+                return true
+            end
+            # Push r
+            push!(rdq, r)
+            # Push [l]
+            push!(ldq, CLOSE)
+            push!(ldq, l)
+            push!(ldq, OPEN)
+            continue
+        else
+            # Include exhaustion by exploiting that ] is represented by the
+            # negative number CLOSE
+            return l < r
+        end
+    end
+    return false
+end
+
+parse_input(raw) = parse_input(raw, convert_string)
+solve_part1(packets) = solve_part1(packets, lessthan)
+solve_part2(packets) = solve_part2(packets, convert_string, lessthan)
+
+# Alternative (slower) solution: tree with recursion
+
+module Alt
+
+import Base: show
 
 struct InnerNode
     children::Vector{Union{InnerNode, Int}}
@@ -11,49 +87,38 @@ struct InnerNode
     InnerNode(children...) = new(collect(Union{InnerNode, Int}, children))
 end
 
-Input = AbstractVector{InnerNode}
-
-function solve_part1(packets::Input)
-    pairs = Iterators.partition(packets, 2)
-    flags = [Bool(is_ordered(pair...)) for pair in pairs]
-    return sum(findall(flags))
-end
-
-function solve_part2(packets::Input)
-    dividers = convert_string.(["[[2]]", "[[6]]"])
-    extended = vcat(packets, dividers)
-    sorted = sort(extended, lt=is_ordered)
-    return prod(findfirst(==(divider), sorted) for divider in dividers)
-end
-
 function convert_string(string::AbstractString)
     return eval(Meta.parse(replace(string, "[" => "InnerNode(", "]" => ")")))
 end
 
-function is_ordered(left::InnerNode, right::InnerNode)
+function lessthan(left::T, right::T) where {T<:Union{InnerNode, <:Integer}}
+    return compare(left, right) == -1
+end
+
+function compare(left::InnerNode, right::InnerNode)
     lch = left.children
     rch = right.children
     for i in 1:min(length(lch), length(rch))
-        result = is_ordered(lch[i], rch[i])
-        if !isnothing(result)
+        result = compare(lch[i], rch[i])
+        if result != 0
             return result
         end
     end
-    return length(lch) == length(rch) ? nothing : (
-        length(lch) < length(rch) ? true : false
-    )
+    return length(lch) == length(rch) ? 0 : (length(lch) < length(rch) ? -1 : 1)
 end
 
-function is_ordered(left::InnerNode, right::Integer)
-    return is_ordered(left, InnerNode(right))
+function compare(left::InnerNode, right::Integer)
+    return compare(left, InnerNode(right))
 end
 
-function is_ordered(left::Integer, right::InnerNode)
-    return is_ordered(InnerNode(left), right)
+function compare(left::Integer, right::InnerNode)
+    return compare(InnerNode(left), right)
 end
 
-function is_ordered(left::T, right::T) where {T<:Integer}
-    return left == right ? nothing : (left < right)
+function compare(left::T, right::T) where {T<:Integer}
+    return left == right ? 0 : (left < right ? -1 : 1)
 end
+
+end # module Alt
 
 end # module
--- test/day13_test.jl
+++ test/day13_test.jl
@@ -1,6 +1,6 @@
 @testset "Day 13" begin
     @testset "Example input" begin
-        input = Day13.parse_input("""
+        raw = """
             [1,1,3,1,1]
             [1,1,5,1,1]
 
@@ -25,16 +25,32 @@
             [1,[2,[3,[4,[5,6,7]]]],8,9]
             [1,[2,[3,[4,[5,6,0]]]],8,9]
             """
-        )
+        input = Day13.parse_input(raw)
 
         @test Day13.solve_part1(input) == 13
         @test Day13.solve_part2(input) == 140
+
+        # input_alt = Day13.parse_input(raw, Day13.Alt.convert_string)
+
+        # @test Day13.solve_part1(input_alt, Day13.Alt.lessthan) == 13
+        # @test Day13.solve_part2(
+        #     input_alt, Day13.Alt.convert_string, Day13.Alt.lessthan,
+        # ) == 140
+
     end
 
     @testset "Real input" begin
-        input = Day13.parse_input(load_input("day13.txt"))
+        raw = load_input("day13.txt")
+        input = Day13.parse_input(raw)
 
         @test Day13.solve_part1(input) == 4734
         @test Day13.solve_part2(input) == 21836
+
+        # input_alt = Day13.parse_input(raw, Day13.Alt.convert_string)
+
+        # @test Day13.solve_part1(input_alt, Day13.Alt.lessthan) == 4734
+        # @test Day13.solve_part2(
+        #     input_alt, Day13.Alt.convert_string, Day13.Alt.lessthan
+        # ) == 21836
     end
 end
