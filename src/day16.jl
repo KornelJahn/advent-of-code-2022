1622064cf68465610031c118b0ef46872892466e
Kornel Jahn <kjahn.public@gmail.com>
2022-12-16 11:57:07 +0100

    day16: Add solution skeleton without algorithm for visiting graph vertices

--- src/day16.jl
+++ src/day16.jl
@@ -1,17 +1,58 @@
 module Day16
 
 function parse_input(raw::AbstractString)
-    return nothing
+    lines = split(strip(raw), '\n')
+    valve_count = length(lines)
+    valvedata = parse_line.(lines)
+    valveindices = Dict{String, Int}()
+    flowrates = Vector{Int}(undef, valve_count)
+    for (i, (name, rate, _)) in enumerate(valvedata)
+        flowrates[i] = rate
+        valveindices[name] = i
+    end
+    neighbors = Vector{Vector{Int}}(undef, valve_count)
+    for (i, (_, _, valve_neighbors)) in enumerate(valvedata)
+        neighbors[i] = [valveindices[n] for n in valve_neighbors]
+    end
+    return flowrates, neighbors
 end
 
-Input = Any
+Input = Tuple{
+    <:AbstractVector{<:Integer},
+    <:AbstractVector{<:AbstractVector{<:Integer}}
+}
 
-function solve_part1(::Input)
-    return nothing
+function solve_part1(input::Input)
+    (flowrates, neighbors) = input
+    new_pressures = collect(visit(input))
+    return sum(accumulate(+, new_pressures, init=0))
 end
 
 function solve_part2(::Input)
     return nothing
+end
+
+function parse_line(line::AbstractString)
+    regex = Regex(string(
+        raw"^Valve ([A-Z]+) has flow rate=(\d+); ",
+        raw"tunnels? leads? to valves? ([A-Z, ]+)$"
+    ))
+    captures = match(regex, line).captures
+    valve_name = captures[1]
+    flow_rate = parse(Int, captures[2])
+    neighbors = split(captures[3], ", ")
+    return valve_name, flow_rate, neighbors
+end
+
+visit(input::Input) = Channel{Int}() do ch
+    (flowrates, neighbors) = input
+    current = 1
+    # TODO: preallocate?
+    visited = Vector{Int}()
+    for t in 1:30
+        # TODO: decide algorithm
+        put!(ch, flowrates[1])
+    end
 end
 
 end # module
--- test/day16_test.jl
+++ test/day16_test.jl
@@ -1,11 +1,20 @@
 @testset "Day 16" begin
     @testset "Example input" begin
-        # input = Day16.parse_input("""
-        #     TODO
-        #     """
-        # )
+        input = Day16.parse_input("""
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II
+            """
+        )
 
-        # @test Day16.solve_part1(input) ==
+        @test Day16.solve_part1(input) == 1651
         # @test Day16.solve_part2(input) ==
     end
 
