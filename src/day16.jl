05492ae7f3cf88e4320ae5606a47411400eadae1
Kornel Jahn <kjahn.public@gmail.com>
2022-12-18 21:58:38 +0100

    Solve day 16

--- src/day16.jl
+++ src/day16.jl
@@ -1,9 +1,13 @@
 module Day16
 
+using DataStructures
+
 function parse_input(raw::AbstractString)
     lines = split(strip(raw), '\n')
     valve_count = length(lines)
-    valvedata = parse_line.(lines)
+    # Do lexicographic ordering on valve names, so AA valve comes first
+    valvedata = sort(parse_line.(lines), by=tup->tup[1])
+    valvenames = Vector{String}(undef, valve_count)
     valveindices = Dict{String, Int}()
     flowrates = Vector{Int}(undef, valve_count)
     for (i, (name, rate, _)) in enumerate(valvedata)
@@ -22,14 +26,27 @@ Input = Tuple{
     <:AbstractVector{<:AbstractVector{<:Integer}}
 }
 
-function solve_part1(input::Input)
-    (flowrates, neighbors) = input
-    new_pressures = collect(visit(input))
-    return sum(accumulate(+, new_pressures, init=0))
+struct State
+    valveidx::Int
+    valvestates::UInt64
+    timeleft::Int
+    playeridx::Int
 end
 
-function solve_part2(::Input)
-    return nothing
+function solve_part1(input::Input)
+    (flowrates, neighbors) = input
+    @assert sum(1 for rate in flowrates if rate != 0) <= 64
+    lut = Dict{State, Int}()
+    state = get_initial_state()
+    return total_pressure!(lut, state, flowrates, neighbors)
+end
+
+function solve_part2(input::Input)
+    (flowrates, neighbors) = input
+    @assert sum(1 for rate in flowrates if rate != 0) <= 64
+    lut = Dict{State, Int}()
+    state = get_initial_state(playeridx=2, multiplay=true)
+    return total_pressure!(lut, state, flowrates, neighbors)
 end
 
 function parse_line(line::AbstractString)
@@ -38,21 +55,99 @@ function parse_line(line::AbstractString)
         raw"tunnels? leads? to valves? ([A-Z, ]+)$"
     ))
     captures = match(regex, line).captures
-    valve_name = captures[1]
-    flow_rate = parse(Int, captures[2])
+    valvename = captures[1]
+    flowrate = parse(Int, captures[2])
     neighbors = split(captures[3], ", ")
-    return valve_name, flow_rate, neighbors
+    return valvename, flowrate, neighbors
 end
 
-visit(input::Input) = Channel{Int}() do ch
-    (flowrates, neighbors) = input
-    current = 1
-    # TODO: preallocate?
-    visited = Vector{Int}()
-    for t in 1:30
-        # TODO: decide algorithm
-        put!(ch, flowrates[1])
+function get_initial_state(;
+    playeridx::Integer=1,
+    multiplay::Bool=false,
+    valvestates::UInt64=zero(UInt64),
+)
+    if multiplay
+        return State(1, valvestates, 26, playeridx)
+    else
+        @assert playeridx == 1
+        return State(1, valvestates, 30, 1)
     end
+end
+
+# Use a LUT, so previously evaluated states do not get re-evaluated in the
+# recursion (dynamic programming).
+function total_pressure!(
+    lut::Dict{State, Int},
+    state::State,
+    flowrates::AbstractVector{<:Integer},
+    neighbors::AbstractVector{<:AbstractVector{<:Integer}}
+)
+    i = state.valveidx
+    valvestates = state.valvestates
+    timeleft = state.timeleft
+    playeridx = state.playeridx
+
+    # Maximal pressure is achieved if two players work simultaneously but open
+    # different valves. From the viewpoint of the optimum, it makes no
+    # difference if players work simultaneously or sequentially. Simulate the
+    # latter as it is easy to add to the recursion.
+    if timeleft == 0
+        if playeridx > 1
+            return total_pressure!(
+                lut,
+                get_initial_state(
+                    playeridx=playeridx-1,
+                    multiplay=true,
+                    valvestates=valvestates,
+                ),
+                flowrates,
+                neighbors,
+            )
+        else
+            return 0
+        end
+    end
+
+    if haskey(lut, state)
+        return lut[state]
+    end
+
+    # Get (i - 1)-th bit (1 for open)
+    is_open = valvestates & (1 << (i - 1)) != 0
+    flowrate = flowrates[i];
+
+    result = 0
+
+    # Calculate total pressure when opening the valve (if it is working)
+    if !is_open && flowrate > 0
+        # Set (i - 1)-th bit to 1 (open)
+        new_valvestates = valvestates | (1 << (i - 1))
+        result = max(
+            result,
+            flowrate * (timeleft - 1) + total_pressure!(
+                lut,
+                State(i, new_valvestates, timeleft - 1, playeridx),
+                flowrates,
+                neighbors,
+            )
+        )
+    end
+    # Calculate total pressure when not touching the valve but moving on to
+    # the neighboring valves
+    for j in neighbors[i]
+        result = max(
+            result,
+            total_pressure!(
+                lut,
+                State(j, valvestates, timeleft - 1, playeridx),
+                flowrates,
+                neighbors,
+            )
+        )
+    end
+
+    lut[state] = result
+    return result
 end
 
 end # module
