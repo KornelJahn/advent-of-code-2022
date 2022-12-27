5e9ab30ba14a77203d164fe7b36b08f0fa4898dc
Kornel Jahn <kjahn.public@gmail.com>
2022-12-27 09:18:49 +0100

    Solve day18 part 1

--- src/day18.jl
+++ src/day18.jl
@@ -1,17 +1,54 @@
 module Day18
 
 function parse_input(raw::AbstractString)
-    return nothing
+    lines = split(strip(raw), '\n')
+    coords = Array{Int, 2}(undef, 3, length(lines))
+    coords[:] .= Iterators.flatten(
+        parse.(Ref(Int), split(line, ',')) for line in lines
+    )
+    return coords
 end
 
-Input = Any
+Input = AbstractArray{<:Integer, 2}
 
-function solve_part1(::Input)
-    return nothing
+function solve_part1(coords::Input)
+    return surface_area(coords)
 end
 
-function solve_part2(::Input)
-    return nothing
+function solve_part2(coords::Input)
+    return exterior_surface_area(coords)
+end
+
+function surface_area(coords::Input)
+    area = 0
+    for i in 0:2
+        indices = mod.([i, i+1, i+2], 3) .+ 1
+        sortedcoords = sortslices(coords[indices, :], dims=2)
+        cols = eachcol(sortedcoords)
+        for (t1, t2) in zip(cols, Iterators.drop(cols, 1))
+            if !(t2[1] == t1[1] && t2[2] == t1[2] && t2[3] == t1[3] + 1)
+                area += 2
+            end
+        end
+        area += 2
+    end
+    return area
+end
+
+function exterior_surface_area(coords::Input)
+    area = 0
+    for i in 0:2
+        indices = mod.([i, i+1, i+2], 3) .+ 1
+        sortedcoords = sortslices(coords[indices, :], dims=2)
+        cols = eachcol(sortedcoords)
+        for (t1, t2) in zip(cols, Iterators.drop(cols, 1))
+            if !(t2[1] == t1[1] && t2[2] == t1[2])
+                area += 2
+            end
+        end
+        area += 2
+    end
+    return area
 end
 
 end # module
--- test/day18_test.jl
+++ test/day18_test.jl
@@ -1,18 +1,30 @@
 @testset "Day 18" begin
     @testset "Example input" begin
-        # input = Day18.parse_input("""
-        #     TODO
-        #     """
-        # )
+        input = Day18.parse_input("""
+            2,2,2
+            1,2,2
+            3,2,2
+            2,1,2
+            2,3,2
+            2,2,1
+            2,2,3
+            2,2,4
+            2,2,6
+            1,2,5
+            3,2,5
+            2,1,5
+            2,3,5
+            """
+        )
 
-        # @test Day18.solve_part1(input) ==
-        # @test Day18.solve_part2(input) ==
+        @test Day18.solve_part1(input) == 64
+        @test Day18.solve_part2(input) == 58
     end
 
     @testset "Real input" begin
-        # input = Day18.parse_input(load_input("day18.txt"))
+        input = Day18.parse_input(load_input("day18.txt"))
 
-        # @test Day18.solve_part1(input) ==
+        @test Day18.solve_part1(input) == 4636
         # @test Day18.solve_part2(input) ==
     end
 end
